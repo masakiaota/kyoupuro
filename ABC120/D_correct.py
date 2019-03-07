0901f9a67bf09a67af2cf55df8965603233a18c7
MasakiAOTA <aotamasakimail@gmail.com>
2019-03-07 20:18:10 +0900

    [WIP] get answer. did class union_find

---
+++ ABC120/D_correct.py
@@ -0,0 +1,49 @@
+def readln():
+    return list(map(int, input().split()))
+
+
+N, M = readln()
+AB = [readln() for _ in range(M)]
+A = [ab[0] for ab in AB]
+B = [ab[1] for ab in AB]
+
+
+class union_find:
+    def __init__(self, N):
+        self.N = N  # ノード数
+        # 親ノードをしめす。負は自身が親ということ。
+        self.parent = [-1] * N  # idxが各ノードに対応。
+
+    def root(self, A):
+        # ノード番号を受け取って一番上の親ノードの番号を帰す
+        if (self.parent[A] < 0):
+            return A
+        self.parent[A] = self.root(A)  # 経由したノードすべての親を上書き
+        return self.parent[A]
+
+    def size(self, A):
+        # ノード番号を受け取って、そのノードが含まれている集合のサイズを返す。
+        return -self.parent[self.root(A)]
+
+    def concat(self, A, B):
+        # ノード番号を2つ受け取って、そのノード同士をつなげる処理を行う。
+        # 引数のノードを直接つなぐ代わりに、親同士を連結する処理にする。
+        A = self.root(A)
+        B = self.root(B)
+
+        # すでにくっついている場合
+        if (A == B):
+            return False
+
+        # 大きい方に小さい方をくっつけたほうが処理が軽いので大小比較
+        if (self.size(A) < self.size(B)):
+            A, B = B, A
+
+        # くっつける
+        self.parent[A] += self.parent[B]  # sizeの更新
+        self.parent[B] = A  # self.rootが呼び出されればBにくっついてるノードもすべて親がAだと上書きされる
+
+        return True
+
+
+ans = []
