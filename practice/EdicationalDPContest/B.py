5ab7e82a9b588592cc05368b3c83f2e495b0923d
Masaki AOTA <aotamasakimail@gmail.com>
2019-04-20 00:41:31 +0900

    practice

--- practice/EdicationalDPContest/B.py
+++ practice/EdicationalDPContest/B.py
@@ -6,23 +6,35 @@ def read_ints():
     return list(map(int, read().split()))
 
 
-import numpy as np
+# import numpy as np
 N, K = read_ints()
-H = np.array(read_ints())
-K = min(len(H), K)
+# H = np.array(read_ints())
+# K = min(len(H), K)
 
-dp = np.full((N), np.inf)
+# dp = np.full((N), np.inf)
+# dp[0] = 0
+# # Kまで初期化しておかないと厄介なことになる
+# # i-kのコストは直接飛ぶのが必ず最小になる
+# dp[1:K+1] = np.abs(H[0]-H[1:K+1])
+
+# for i in range(K+1, N):
+#     # i-Kまでのコストリストを作成しておく
+#     dp[i] = (dp[(i - K):(i)] + np.abs(H[i] - H[(i - K):(i)])).min()
+
+
+# print(int(dp[-1]))
+
+
+# 配るDPでもやる
+# pypy3で実行しないとTLEになる
+
+K = min(N, K)
+H = read_ints() + [0 for _ in range(K)]
+dp = [float('inf') for _ in range(N+K)]
 dp[0] = 0
-# Kまで初期化しておかないと厄介なことになる
-# i-kのコストは直接飛ぶのが必ず最小になる
-dp[1:K+1] = np.abs(H[0]-H[1:K+1])
+for i in range(N):
+    # 配るDPでやる
+    for k in range(1, K+1):
+        dp[i + k] = min(dp[i + k], dp[i] + abs(H[i] - H[i + k]))
 
-for i in range(K+1, N):
-    # i-Kまでのコストリストを作成しておく
-    dp[i] = (dp[(i - K):(i)] + np.abs(H[i] - H[(i - K):(i)])).min()
-
-
-print(int(dp[-1]))
-
-
-# 配るDPでもやってみたい
+print(int(dp[N-1]))
--- practice/EdicationalDPContest/C.py
+++ practice/EdicationalDPContest/C.py
@@ -39,3 +39,49 @@ def read_col(H, n_cols):
             ret[col].append(tmp[col])
 
     return ret
+
+# 全探索だと
+# 3^N通りになってTLEだこれ
+
+# 制限がなければ、iごとにmaxを取っておわりO(N)
+
+# iの時の最善の状態が[i,j](jは行動)のとき、i+1最善の状態は[i+1,k](k!=j)となる
+# これはDPで実装できそう！
+
+
+# import numpy as np
+
+# N = read_a_int()
+# ABC = np.array(read_matrix(N))
+# # A, B, C = read_col(N, 3)
+
+# dp = np.full((N, 3), 0, dtype='int16')
+# # 0->a,1->b,2->c
+
+# dp[0, :] = ABC[0]
+
+# for i in range(N - 1):
+#     for j, abc in enumerate(ABC[i+1]):
+#         dp[i + 1, j] = dp[i, [k for k in range(3) if k != j]].max() + abc
+
+# # print(dp)
+# print(dp[-1].max())
+
+# TLEしたのでpypy3仕様にかきなおし
+
+N = read_a_int()
+ABC = read_matrix(N)
+
+# dp = np.full((N, 3), -float('inf'))
+dp = [[-float('inf') for _ in range(3)] for _ in range(N)]
+# 0->a,1->b,2->c
+
+for i in range(3):
+    dp[0][i] = ABC[0][i]
+
+for i in range(N - 1):
+
+    for j in range(3):
+        dp[i+1][j] = max([dp[i][k] + ABC[i+1][j] for k in range(3) if k != j])
+
+print(int(max(dp[-1])))
