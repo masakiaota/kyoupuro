737a00edca400c36b52273e4bee29acbe8a42616
Masaki AOTA <aotamasakimail@gmail.com>
2019-04-20 23:26:13 +0900

    my answers . C is WA, C.1 is correct

---
+++ contests/Tenka1BC2019/D.py
@@ -0,0 +1,56 @@
+# 入力が10**5とかになったときに100ms程度早い
+import sys
+read = sys.stdin.readline
+
+
+def read_ints():
+    return list(map(int, read().split()))
+
+
+def read_a_int():
+    return int(read())
+
+
+def read_matrix(H):
+    '''
+    H is number of rows
+    '''
+    return [list(map(int, read().split())) for _ in range(H)]
+
+
+def read_map(H):
+    '''
+    H is number of rows
+    文字列で与えられた盤面を読み取る用
+    '''
+    return [read() for _ in range(H)]
+
+
+def read_col(H, n_cols):
+    '''
+    H is number of rows
+    n_cols is number of cols
+
+    A列、B列が与えられるようなとき
+    '''
+    ret = [[] for _ in range(n_cols)]
+    for _ in range(H):
+        tmp = list(map(int, read().split()))
+        for col in range(n_cols):
+            ret[col].append(tmp[col])
+
+    return ret
+
+
+N = read_a_int()
+
+A = [int(read()) for _ in range(N)]
+
+MOD = 998244353
+
+# totalを導入すると
+# あるグループ(total -2グループの合計) > 他2グループ できない
+
+# グループはr,g,bに分けられているが問題の対称性からこれらを区別なく扱ったほうが効率が良い
+
+#
