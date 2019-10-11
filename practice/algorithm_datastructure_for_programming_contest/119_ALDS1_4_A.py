0032693648f90b372a9c6ab625c99de459727575
Masaki AOTA <aotamasakimail@gmail.com>
2019-10-11 19:36:43 +0900

    rewrite smater

--- practice/algorithm_datastructure_for_programming_contest/119_ALDS1_4_A.py
+++ practice/algorithm_datastructure_for_programming_contest/119_ALDS1_4_A.py
@@ -1,4 +1,4 @@
-# https: // onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_A
+# https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_A
 # やるだけ # 本では番兵を用いた実装だと定数倍早いとあるが、pythonではwhileが遅いのでおとなしくfor文を書く
 
 n = input()
@@ -7,9 +7,16 @@ q = input()
 T = list(map(int, input().split()))
 
 cnt = 0
+# for t in T:
+#     for s in S:
+#         if t == s:
+#             cnt += 1
+#             break
+# こんなのを書かなくももっと簡単にかける
+
 for t in T:
-    for s in S:
-        if t == s:
-            cnt += 1
-            break
+    if t in S:
+        cnt += 1
+
 print(cnt)
+# list.index(hoge)とかlist.count(hoge)とかもよく使う。
