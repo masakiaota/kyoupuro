3d72aad9909af9cd01338d44b995ed8946766d8b
Masaki AOTA <aotamasakimail@gmail.com>
2019-10-23 00:26:00 +0900

    adj mat

--- practice/algorithm_datastructure_for_programming_contest/269_ALDS1_11_A.py
+++ practice/algorithm_datastructure_for_programming_contest/269_ALDS1_11_A.py
@@ -1,3 +1,14 @@
 # https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/11/ALDS1_11_A
 # やるだけ
 # 有向成分を読み込むたびにノード番号に対応する表に書き込んでいけばよい
+
+N = int(input())
+for _ in range(N):
+    out_ls = [0]*N
+    tmp = list(map(int, input().split()))
+    # id = tmp[0]-1 #idは順番通り与えられるので実はこれもいらない
+    if tmp[1] != 0:
+        for adj in tmp[2:]:
+            out_ls[adj-1] = 1
+
+    print(*out_ls)
