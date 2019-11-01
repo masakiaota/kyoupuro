5fe139cf11d82d25ec7fc370716d7c8939d3760a
MasakiAOTA <aotamasakimail@gmail.com>
2019-11-01 21:39:11 +0900

    WIP プリムのアルゴリズム

---
+++ practice/algorithm_datastructure_for_programming_contest/296_ALDS1_12_A.py
@@ -0,0 +1,3 @@
+# https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/12/ALDS1_12_A
+# 本よりこっちのほうがぶっちゃけわかりやすい http://www.deqnotes.net/acmicpc/prim/
+# 螺旋本ではダイクストラとの違いをあまり解説していないが、違いはダイクストラのところで考えよう
