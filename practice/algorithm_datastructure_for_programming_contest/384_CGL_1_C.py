03af3af06e8e030d4d4998199253425e46ecab4a
Masaki AOTA <aotamasakimail@gmail.com>
2019-11-22 22:56:46 +0900

    WIP 384

---
+++ practice/algorithm_datastructure_for_programming_contest/384_CGL_1_C.py
@@ -0,0 +1,66 @@
+# https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_1_C
+# この問題は後の交差判定のところで使う
+from math import sqrt
+
+
+class Vector:
+    def __init__(self, ls):
+        '''
+        ls ... list
+        '''
+        self.vec = ls
+
+    def __len__(self):
+        return len(self.vec)
+
+    def __getitem__(self, idx):
+        return self.vec[idx]
+
+    def __repr__(self):
+        return f'Vector({self.vec})'
+
+    def add(self, vec):
+        '''
+        vec ... vector class
+        '''
+        assert len(self) == len(vec)
+        ret = [a + b for a, b in zip(self.vec, vec.vec)]
+        return Vector(ret)
+
+    def sub(self, vec):
+        '''
+        vec ... vector class
+        '''
+        assert len(self) == len(vec)
+        ret = [a - b for a, b in zip(self.vec, vec.vec)]
+        return Vector(ret)
+
+    def dot(self, vec):
+        '''
+        vec ... vector class
+        '''
+        assert len(self) == len(vec)
+        ret = [a * b for a, b in zip(self.vec, vec.vec)]
+        return Vector(ret)
+
+    def norm(self):
+        tmp = sum([x * x for x in self.vec])
+        return sqrt(tmp)
+
+
+def norm(vec):
+    '''
+    vec ... Vector class
+    '''
+    return vec.norm()
+
+
+def cross(a, b):
+    '''
+    Outer product for 2d
+    a,b ... Vector class
+    '''
+    assert len(a) == 2 and len(b) == 2
+    first = a[0] * b[1]
+    second = a[1] * b[0]
+    return first - second
---
+++ practice/algorithm_datastructure_for_programming_contest/387_CGL_2_B.py
@@ -0,0 +1,2 @@
+# https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_2_B
+# この問題は後の店の内包に繋がります
