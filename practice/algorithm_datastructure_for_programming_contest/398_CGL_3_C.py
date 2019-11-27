e09f5e57d96978207744a81873556b2f311b57f9
Masaki AOTA <aotamasakimail@gmail.com>
2019-11-27 23:55:56 +0900

    書きかけ

--- practice/algorithm_datastructure_for_programming_contest/398_CGL_3_C.py
+++ practice/algorithm_datastructure_for_programming_contest/398_CGL_3_C.py
@@ -1,2 +1,83 @@
 # https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_3_C
 # 本の説明を図に書き出してみるとよく理解できる。
+from math import sqrt
+
+
+class Vector:
+    def __init__(self, ls):
+        '''
+        ls ... list
+        '''
+        self.vec = ls
+
+    def __len__(self):
+        return len(self.vec)
+
+    def __getitem__(self, idx):
+        return self.vec[idx]
+
+    def __repr__(self):
+        return f'Vector({self.vec})'
+
+    def add(self, vec):
+        '''
+        vec ... vector class
+        '''
+        assert len(self) == len(vec)
+        ret = [a + b for a, b in zip(self.vec, vec.vec)]
+        return Vector(ret)
+
+    def sub(self, vec):
+        '''
+        vec ... vector class
+        '''
+        assert len(self) == len(vec)
+        ret = [a - b for a, b in zip(self.vec, vec.vec)]
+        return Vector(ret)
+
+    def mul(self, vec):
+        '''
+        vec ... vector class
+        '''
+        assert len(self) == len(vec)
+        ret = [a * b for a, b in zip(self.vec, vec.vec)]
+        return Vector(ret)
+
+    def norm(self):
+        tmp = sum([x * x for x in self.vec])
+        return sqrt(tmp)
+
+
+def norm(vec):
+    '''
+    vec ... Vector class
+    '''
+    return vec.norm()
+
+
+def cross(a, b):
+    '''
+    Outer product for 2d
+    a,b ... Vector class
+    '''
+    assert len(a) == 2 and len(b) == 2
+    first = a[0] * b[1]
+    second = a[1] * b[0]
+    return first - second
+
+
+def dot(a, b):
+    return sum(a.mul(b))
+
+
+EPS = 1e-10
+
+
+def contains(G, p):
+    '''
+    Gは多角形を表すリストで、今回ならばvectorが中に並んでいることとする
+    pは内容しているか判別したい点で、Vectorで表す
+    pが多角形Gに内包されていればTrueを返す
+    '''
+    ret = False
+    for i in G
