25d715749f9e2a52dea7ff9bc12d7e149e3d975d
MasakiAOTA <aotamasakimail@gmail.com>
2019-11-30 14:58:48 +0900

    点の内包を調べる。

--- practice/algorithm_datastructure_for_programming_contest/398_CGL_3_C.py
+++ practice/algorithm_datastructure_for_programming_contest/398_CGL_3_C.py
@@ -77,7 +77,38 @@ def contains(G, p):
     '''
     Gは多角形を表すリストで、今回ならばvectorが中に並んでいることとする
     pは内容しているか判別したい点で、Vectorで表す
-    pが多角形Gに内包されていればTrueを返す
+
+    pが多角形Gの辺上にあれば1
+    pが多角形Gに内包されていれば2
+    それ以外は0をreturnする
     '''
-    ret = False
-    for i in G
+    is_contain = False  # 内包してるか
+    for i in range(len(G)):
+        a = G[i].sub(p)
+        b = G[(i + 1) % len(G)].sub(p)  # i+1が0に戻るようにこのような形式にしている。
+        # もしpがG[i]とG[i+1]の線分上にある場合は即座に1をreturnします
+        if abs(cross(a, b)) < EPS and dot(a, b) < EPS:
+            # 外積が0→a,bが同一直線上
+            # かつ 内積が負→a,bは逆を向いている
+            # ならばpは線分上に存在する
+            return 1
+        # 内包を判定する。前処理として、yの座標によってa,bを入れ替える
+        if a[1] > b[1]:  # aの方のy座標を小さくしたい
+            a, b = b, a
+        if a[1] < EPS and b[1] > EPS and cross(a, b) > EPS:  # 実際に判別する
+            is_contain = (not is_contain)
+    return (0, 2)[is_contain]  # 書き方キモいけど三項演算子の短い書き方
+
+
+# load data
+N = int(input())
+G = []
+for _ in range(N):
+    g = Vector(list(map(int, input().split())))
+    G.append(g)
+
+# answer query
+Q = int(input())
+for _ in range(Q):
+    p = Vector(list(map(int, input().split())))
+    print(contains(G, p))
