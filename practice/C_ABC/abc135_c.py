10ebaef5b5c9ee2d78bf0bb1f5f0052194e6a43f
Masaki AOTA <aotamasakimail@gmail.com>
2019-12-25 00:59:59 +0900

    こんがらがりがち

---
+++ practice/C_ABC/abc135_c.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/abc135/tasks/abc135_c
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+ans = 0
+l = A[0]
+for i in range(N):
+    r = A[i + 1]
+    cur = B[i]
+    # print(l, r, cur)
+    ans += min(l + r, cur)
+    l = max(r - max(cur - l, 0), 0)  # めちゃくちゃ整理するとこの形の式になる
+print(ans)
