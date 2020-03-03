cc6b4a0d0ef1c660f1dd505095597649c4686f89
MasakiAOTA <aotamasakimail@gmail.com>
2020-03-03 16:47:56 +0900

    seg木下書き

--- algorithm/SegmentTree.py
+++ algorithm/SegmentTree.py
@@ -1,33 +1,62 @@
 # 下書き
 # https://juppy.hatenablog.com/entry/2019/05/02/%E8%9F%BB%E6%9C%AC_python_%E3%82%BB%E3%82%B0%E3%83%A1%E3%83%B3%E3%83%88%E6%9C%A8_%E7%AB%B6%E6%8A%80%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0_Atcoder
 
-# ここではMRQ(minimum range query)を想定する
-
 
 class SegmentTree:
-    def __init__(self, ls: list, segfunc, outer):
+    def __init__(self, ls: list, segfunc, identity_element):
         '''
         セグ木 (下書き)
-        一次元のリストlsを受け取り初期化する。
+        一次元のリストlsを受け取り初期化する。O(len(ls))
         区間のルールはsegfuncによって定義される
-        outerは区間外に設定する値。e.g., 最小値を求めたい→inf, 和→0, 積→1, gcd→0
+        identity elementは単位元。e.g., 最小値を求めたい→inf, 和→0, 積→1, gcd→0
+        [単位元](https://ja.wikipedia.org/wiki/%E5%8D%98%E4%BD%8D%E5%85%83)
         '''
+        self.ide = identity_element
+        self.func = segfunc
         n = len(ls)
         self.num = 2 ** (n - 1).bit_length()  # n以上の最小の2のべき乗
-        self.tree = [outer] * (2 * self.num - 1)  # −1はぴったりに作るためだけど気にしないでいい
+        self.tree = [self.ide] * (2 * self.num - 1)  # −1はぴったりに作るためだけど気にしないでいい
         for i, l in enumerate(ls):  # 木の葉に代入
             self.tree[i + self.num - 1] = l
-        for i in range(self.num - 2, -1, -1):
-            # 子を束ねて親を更新
+        for i in range(self.num - 2, -1, -1):  # 子を束ねて親を更新
             self.tree[i] = segfunc(self.tree[2 * i + 1], self.tree[2 * i + 2])
 
     def update(self, i, x):
-        return NotImplementedError()
+        '''
+        i番目の要素をxに変更する(木の中間ノードも更新する) O(logN)
+        '''
+        i += self.num - 1
+        self.tree[i] = x
+        while i:  # 木を更新
+            i = (i - 1) // 2
+            self.tree[i] = self.func(self.tree[i * 2 + 1],
+                                     self.tree[i * 2 + 2])
 
     def query(self, l, r):
-        return NotImplementedError()
+        '''
+        区間[l,r)に対するクエリをO(logN)で処理する。例えばその区間の最小値、最大値、gcdなど
+        '''
+        if r <= l:
+            return ValueError('invalid index (l,rがありえないよ)')
+        l += self.num - 1
+        r += self.num - 2  # ここで半開区間でなく[l,r]で以下を処理する
+        res = self.ide
+        while r - l > 1:  # 右から寄りながら結果を結合していくイメージ
+            if l & 1 == 0:
+                res = self.func(res, self.tree[l])
+            if r & 1 == 1:
+                res = self.func(res, self.tree[r])
+                r -= 1
+            l = l // 2  # 親の一つ右に移動
+            r = (r - 1) // 2  # 親の一つ左に移動
+        if l == r:
+            res = self.func(res, self.tree[l])
+        else:
+            res = self.func(res, self.tree[l])
+            res = self.func(res, self.tree[r])
+        return res
 
 
-def segfunc():
+def segfunc(x, y):
     # 処理したい内容
-    return
+    return min(x, y)  # 例えばね
