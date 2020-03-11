e89a711e80e9c5eff30adf8a8034e7c529c0fb43
Masaki AOTA <aotamasakimail@gmail.com>
2020-03-11 23:26:50 +0900

    add lazy seg tree

--- algorithm/SegmentTree.py
+++ algorithm/SegmentTree.py
@@ -78,14 +78,37 @@ print(st.query(7, 11))
 # 大丈夫そう
 
 
-# http://tsutaj.hatenablog.com/entry/2017/03/30/224339
 # 遅延評価セグメント木
-# 区間和クエリを扱うのを例に
-import operator
+# http://tsutaj.hatenablog.com/entry/2017/03/30/224339 アイデアはわかりやすいけど再帰関数なので避けた
+# 区間和クエリを扱うのを例に (あとであとで実装)
+
+# 参考
+# https://smijake3.hatenablog.com/entry/2018/11/03/100133
+# こっちのほうがわかりやすいかもね
+
+def _gidx(l, r, treesize):
+    '''
+    lazy propagation用idx生成器 木の下から生成される。1based-indexなので注意.(使うときは-1するとか)
+    もとの配列において[l,r)を指定したときに更新すべきidxをyieldする
+    treesizeは多くの場合self.num
+    '''
+    L, R = l + treesize, r + treesize
+    lm = (L // (L & -L)) >> 1  # これで成り立つの天才か？
+    rm = (R // (R & -R)) >> 1
+    while L < R:
+        if R <= rm:
+            yield R
+        if L <= lm:
+            yield L
+        L >>= 1
+        R >>= 1
+    while L:  # Rでもいいけどね
+        yield L
+        L >>= 1
 
 
-class SegmentTreeForSum:
-    def __init__(self, ls: list, identity_element):
+class SegmentTreeForRMQ:  # range minimum query
+    def __init__(self, ls: list, segfunc=min, identity_element=2**63, lazy_ide=None):
         '''
         セグ木
         一次元のリストlsを受け取り初期化する。O(len(ls))
@@ -94,50 +117,67 @@ class SegmentTreeForSum:
         [単位元](https://ja.wikipedia.org/wiki/%E5%8D%98%E4%BD%8D%E5%85%83)
         '''
         self.ide = identity_element
-        self.func = operator.add
+        self.lide = lazy_ide  # lazy用単位元
+        self.func = segfunc
         n = len(ls)
         self.num = 2 ** (n - 1).bit_length()  # n以上の最小の2のべき乗
-        self.tree = [self.ide] * (2 * self.num - 1)
-        self.lazy = [self.ide] * (2 * self.num - 1)  # 遅延配列
+        self.tree = [self.ide] * (2 * self.num)  # 関係ない値を-1においてアクセスを許すと都合が良い
+        self.lazy = [self.lide] * (2 * self.num)  # 遅延配列
         for i, l in enumerate(ls):  # 木の葉に代入
             self.tree[i + self.num - 1] = l
         for i in range(self.num - 2, -1, -1):  # 子を束ねて親を更新
             self.tree[i] = segfunc(self.tree[2 * i + 1], self.tree[2 * i + 2])
 
-    def _lazyeval(self, i, l, r):
+    def _lazyprop(self, *ids):
         '''
         遅延評価用の関数
         - self.tree[i] に self.lazy[i]の値を伝播させて遅延更新する
         - 子ノードにself.lazyの値を伝播させる **ここは問題ごとに書き換える必要がある**
-        - self.tree[i]をリセットする
+        - self.lazy[i]をリセットする
         '''
-        lazyi = self.lazy[i]
-        if lazyi == self.ide:
-            return  # 値を伝播する必要がない場合は即時終了
-        self.tree[i] = self.func(self.tree[i], lazyi)
-        if r - l > 1:  # r-lが1より離れていれば最下段ではない(半開区間)
-            # 子へ遅延値を伝播させていく
-            # ここのルールは問題によって異なる クエリが区間和の場合はこう
-            self.lazy[2 * i + 1] += lazyi // 2
-            self.lazy[2 * i + 2] += lazyi // 2
-        self.lazy[i] = self.ide  # 遅延配列を空に戻す
+        for i in reversed(ids):
+            i -= 1  # 0basedindexに修正
+            v = self.lazy[i]
+            if v == self.lide:
+                continue  # 単位元ならする必要のNASA
+            # どうやって遅延更新するかは問題によってことなる
+            # 今回なら数字書き換えなので数字をそのまま子ノードに伝播
+            # lazyもtreeも書き換える必要あり
+            self.tree[2 * i + 1] = v
+            self.tree[2 * i + 2] = v
+            self.lazy[2 * i + 1] = v
+            self.lazy[2 * i + 2] = v
+            self.lazy[i] = self.lide  # 遅延配列を空に戻す
 
-    def update(self, i, x):
+    def update(self, l, r, x):
         '''
-        i番目の要素をxに変更する(木の中間ノードも更新する) O(logN)
+        [l,r)番目の要素をxに変更する(木の中間ノードも更新する) O(logN)
         '''
-        i += self.num - 1
-        self.tree[i] = x
-        while i:  # 木を更新
-            i = (i - 1) // 2
-            self.tree[i] = self.func(self.tree[i * 2 + 1],
-                                     self.tree[i * 2 + 2])
-
-    def update_kukan(self, l, r, x):
-        '''
-        区間[l,r)に対してxをself.funcする(今回なら足し込む)
-        '''
-        eval()
+        # 1, 根から区間内においてlazyの値を伝播しておく(self.treeの値が有効になる)
+        ids = tuple(_gidx(l, r, self.num))
+        self._lazyprop(*ids)
+        # 2, 区間に対してtree,lazyの値を更新 (treeは根方向に更新するため、lazyはpropで葉方向に更新するため)
+        if r <= l:
+            return ValueError('invalid index (l,rがありえないよ)')
+        l += self.num
+        r += self.num
+        while l < r:
+            if r & 1:
+                r -= 1  # 一旦半開ではなくするために
+                self.tree[r - 1] = x
+                self.lazy[r - 1] = x
+            if l & 1:
+                self.tree[l - 1] = x
+                self.tree[l - 1] = x  # ここのセットの仕方も問題によって変えるべし
+                self.lazy[l - 1] = x  # lazyの区間に値をセット
+                l += 1
+            l >>= 1
+            r >>= 1
+        # 3, 伝播させた区間について下からdataの値を伝播する
+        for i in ids:
+            i -= 1  # to 0based
+            self.tree[i] = self.func(
+                self.tree[2 * i + 1], self.tree[2 * i + 2])
 
     def query(self, l, r):
         '''
@@ -145,25 +185,19 @@ class SegmentTreeForSum:
         '''
         if r <= l:
             return ValueError('invalid index (l,rがありえないよ)')
-        l += self.num - 1
-        r += self.num - 2  # ここで半開区間でなく[l,r]で以下を処理する
+        # 1, 根からにlazyの値を伝播させる
+        self._lazyprop(*_gidx(l, r, self.num))
+        # 2, 区間[l,r)の最小値を求める
+        l += self.num
+        r += self.num
         res = self.ide
-        while r - l > 1:  # 右から寄りながら結果を結合していくイメージ
-            if l & 1 == 0:
-                res = self.func(res, self.tree[l])
-            if r & 1 == 1:
-                res = self.func(res, self.tree[r])
+        while l < r:  # 右から寄りながら結果を結合していくイメージ
+            if r & 1:
                 r -= 1
-            l = l // 2  # 親の一つ右に移動
-            r = (r - 1) // 2  # 親の一つ左に移動
-        if l == r:
-            res = self.func(res, self.tree[l])
-        else:
-            res = self.func(res, self.tree[l])
-            res = self.func(res, self.tree[r])
+                res = self.func(res, self.tree[r - 1])
+            if l & 1:
+                res = self.func(res, self.tree[l - 1])
+                l += 1
+            l >>= 1
+            r >>= 1  # 親の一つ左に移動
         return res
-
-
-# 参考
-# https://smijake3.hatenablog.com/entry/2018/11/03/100133
-# こっちのほうがわかりやすいかもね
