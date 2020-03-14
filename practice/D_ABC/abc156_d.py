0e115a402dcd4b2065a518ffed8bc2356e5689f3
Masaki AOTA <aotamasakimail@gmail.com>
2020-03-14 20:37:59 +0900

    debug ModInt

--- practice/D_ABC/abc156_d.py
+++ practice/D_ABC/abc156_d.py
@@ -1,71 +1,12 @@
-def is_prime(x: int):
-    # 高速素数判定
-    if x == 1:
-        return False
-    if x % 2 == 0:
-        if x == 2:
-            return True
-        else:
-            return False
+# https://atcoder.jp/contests/abc156/submissions/10277689
 
-    for i in range(3, int(x**0.5) + 1, 2):
-        if x % i == 0:
-            return False
-
-    return True
+# コンテスト中に通せたけどmodintクラスで実装してみる
+import sys
+read = sys.stdin.readline
 
 
-def ret_erators(N: int):
-    # エラトステネスの篩
-    is_prime = [True] * (N + 1)
-
-    # 0と1は素数ではない
-    is_prime[0] = False
-    is_prime[1] = False
-
-    for i in range(2, int(N**0.5) + 1):
-        if is_prime[i]:
-            j = i * 2  # iの倍数は素数ではない
-            while j < N + 1:
-                is_prime[j] = False
-                j += i
-    return is_prime
-
-
-def factorization(n: int):
-    if n == 1:
-        return []  # 1は素数ではない
-    # 素因数分解
-    arr = []
-    temp = n
-    for i in range(2, int(n**0.5) + 1):  # ここにバグがないか心配
-        if temp % i == 0:
-            cnt = 0
-            while temp % i == 0:
-                cnt += 1
-                temp //= i
-            arr.append((i, cnt))
-
-    if temp != 1:
-        arr.append((temp, 1))
-
-    if arr == []:
-        arr.append((n, 1))
-
-    return arr
-
-
-def make_divisors(n: int, sort=False):
-    # 約数列挙
-    divisors = []
-    for i in range(1, int(n**0.5) + 1):
-        if n % i == 0:
-            divisors.append(i)
-            if i != n // i:
-                divisors.append(n // i)
-    if sort:
-        divisors.sort()
-    return divisors
+def read_ints():
+    return list(map(int, read().split()))
 
 
 class ModInt:
@@ -135,7 +76,6 @@ class ModInt:
 
 
 def combination(n, r):
-    # 上記のModIntを用いた通りの数の実装
     if r > n:
         return 0  # このような通りの数は無いため便宜上こう定義する
     r = min(r, n - r)
@@ -144,3 +84,12 @@ def combination(n, r):
         nf = nf * (n - i)
         rf = rf * (i + 1)
     return nf / rf
+
+
+# すべての通り(2^n-1)からnCa,nCbを引けば良い
+MOD = 10**9 + 7
+n, a, b = read_ints()
+
+tmp = ModInt(2)**n - 1
+ans = tmp - combination(n, a) - combination(n, b)
+print(ans)
