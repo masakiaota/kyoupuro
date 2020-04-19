727faae1e9474d34bea8a690c785d037948512b8
Masaki AOTA <aotamasakimail@gmail.com>
2020-04-19 23:20:54 +0900

    4完unrated おわり

---
+++ contests/abc163/e/e.py
@@ -0,0 +1,105 @@
+import sys
+sys.setrecursionlimit(1 << 25)
+read = sys.stdin.readline
+ra = range
+enu = enumerate
+
+
+def read_ints():
+    return list(map(int, read().split()))
+
+
+def read_a_int():
+    return int(read())
+
+
+def read_tuple(H):
+    '''
+    H is number of rows
+    '''
+    ret = []
+    for _ in range(H):
+        ret.append(tuple(map(int, read().split())))
+    return ret
+
+
+def read_col(H):
+    '''
+    H is number of rows
+    A列、B列が与えられるようなとき
+    ex1)A,B=read_col(H)    ex2) A,=read_col(H) #一列の場合
+    '''
+    ret = []
+    for _ in range(H):
+        ret.append(list(map(int, read().split())))
+    return tuple(map(list, zip(*ret)))
+
+
+def read_matrix(H):
+    '''
+    H is number of rows
+    '''
+    ret = []
+    for _ in range(H):
+        ret.append(list(map(int, read().split())))
+    return ret
+    # return [list(map(int, read().split())) for _ in range(H)] # 内包表記はpypyでは遅いため
+
+
+from heapq import heapify, heappop, heappush, heappushpop
+
+
+class PriorityQueue:
+    def __init__(self, heap):
+        '''
+        heap ... list
+        '''
+        self.heap = heap
+        heapify(self.heap)
+
+    def push(self, item):
+        heappush(self.heap, item)
+
+    def pop(self):
+        return heappop(self.heap)
+
+    def pushpop(self, item):
+        return heappushpop(self.heap, item)
+
+    def __call__(self):
+        return self.heap
+
+    def __len__(self):
+        return len(self.heap)
+
+
+MOD = 10**9 + 7
+INF = 2**31  # 2147483648 > 10**9
+# default import
+from collections import defaultdict, Counter, deque
+from operator import itemgetter
+from itertools import product, permutations, combinations
+from bisect import bisect_left, bisect_right  # , insort_left, insort_right
+
+N = read_a_int()
+A = read_ints()
+# O(NN)解法
+# 大きい方から貪欲に取っていくのは違う
+candi = []
+for i, j in product(range(N), repeat=2):
+    candi.append((A[i] * abs(i - j), A[i], i, j))
+candi.sort()
+
+is_used_i = [False] * N
+is_used_j = [False] * N
+ans = 0
+# print(candi)
+while candi:
+    happiness, a, i, j = candi.pop()
+    if is_used_i[i] or is_used_j[j]:
+        continue
+    # print(happiness, i, j)
+    ans += happiness
+    is_used_i[i] = True
+    is_used_j[j] = True
+print(ans)
