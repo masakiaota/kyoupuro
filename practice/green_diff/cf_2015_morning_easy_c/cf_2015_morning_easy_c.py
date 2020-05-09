abec2c0726657433bfe8ceffc9c40cfd2df711e1
Masaki AOTA <aotamasakimail@gmail.com>
2020-05-09 21:13:12 +0900

    keep on streak!

---
+++ practice/green_diff/cf_2015_morning_easy_c/cf_2015_morning_easy_c.py
@@ -0,0 +1,45 @@
+import sys
+sys.setrecursionlimit(1 << 25)
+read = sys.stdin.readline
+ra = range
+enu = enumerate
+
+
+def read_ints():
+    return list(map(int, read().split()))
+
+
+def read_col(H):
+    '''
+    H is number of rows
+    A列、B列が与えられるようなとき
+    ex1)A,B=read_col(H)    ex2) A,=read_col(H) #一列の場合
+    '''
+    ret = []
+    for _ in range(H):
+        ret.append(list(map(int, read().split())))
+    return tuple(map(list, zip(*ret)))
+
+
+MOD = 10**9 + 7
+INF = 2**31  # 2147483648 > 10**9
+# default import
+from collections import defaultdict, Counter, deque
+from operator import itemgetter
+from itertools import product, permutations, combinations
+from bisect import bisect_left, bisect_right  # , insort_left, insort_right
+
+N, K, M, R = read_ints()
+S, = read_col(N - 1)
+S.sort(reverse=True)
+
+if sum(S[:K]) >= R * K:
+    print(0)
+    exit()
+
+
+x = R * K - sum(S[:K - 1])
+if x > M:
+    print(-1)
+else:
+    print(x)
