541bbf7e1e2fc2a6144cf78dc6d1972f20257cdc
Masaki AOTA <aotamasakimail@gmail.com>
2020-05-17 14:47:10 +0900

    delete useless codes

--- practice/ants_book/056_longest_common_subsequence.py
+++ practice/ants_book/056_longest_common_subsequence.py
@@ -9,14 +9,13 @@ dp[i][j] ... s[:i]とt[:j]のLCS長
 dp[i+1][j+1] = max(dp[i][j+1],dp[j+1][j], dp[i][j]+1) #3項目はs[i]==t[j]のときだけ
 ∵同じ文字だったら最長が1が増える。そうじゃなかったら最長の方を選ぶ
 '''
-INF = 2**31
-dp = [[INF] * (m + 1) for _ in range(n + 1)]
+dp = [[0] * (m + 1) for _ in range(n + 1)]
 
-for i in range(n + 1):
-    dp[i][0] = 0
-
-for j in range(m + 1):
-    dp[0][j] = 0
+# # 初期化を明示的に書くと下のように成るけどすべての要素を0で初期化してるので今はいらない
+# for i in range(n + 1):
+#     dp[i][0] = 0
+# for j in range(m + 1):
+#     dp[0][j] = 0
 
 for i in range(n):
     for j in range(m):
