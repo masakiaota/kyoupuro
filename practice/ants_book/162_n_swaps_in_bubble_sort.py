9b5f8789e244bd09cc4203d939d6f12b10dd60a6
MasakiAOTA <aotamasakimail@gmail.com>
2020-05-27 12:32:29 +0900

    BIT

--- practice/ants_book/162_n_swaps_in_bubble_sort.py
+++ practice/ants_book/162_n_swaps_in_bubble_sort.py
@@ -1,7 +1,7 @@
-# verify 済み
 class BIT:
     def __init__(self, n):
         self.n = n
+        # self.num = 2 ** (self.n_origin - 1).bit_length()  # n以上の最小の2のべき乗
         self.bit = [0] * (self.n + 1)  # bitは(1based indexっぽい感じなので)
 
     def init(self, ls):
@@ -29,3 +29,22 @@ class BIT:
             ret += self.bit[i]
             i -= (i & -i)
         return ret
+
+
+n = 4
+A = [3, 1, 4, 2]
+bit = BIT(max(A) + 1)  # 0~Aの最大までの座標を用意しておく
+ans = 0
+# i<jにおいて,ai>ajとなる要素の個数をカウント
+# jを固定すれば、jより前に出現したajよりも大きい要素の数になる
+for a in A:
+    ans += bit.sum(a + 1, bit.n)  # aより大きい要素の個数が
+    bit.add(a, 1)
+
+    # for a in range(0, max(A) + 1):
+    #     # ちゃんと各要素がなってるかの検証
+    #     print(bit.sum(a, a + 1), end=' ')
+    # print()
+
+
+print(ans)
