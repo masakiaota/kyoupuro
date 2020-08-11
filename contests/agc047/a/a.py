bf4c4b4cb709af9e9298dc8675ad0286293d872b
Masaki AOTA <aotamasakimail@gmail.com>
2020-08-11 20:53:46 +0900

    floatであっても13桁では誤差が出る 文字列で扱うべし

--- contests/agc047/a/a.py
+++ contests/agc047/a/a.py
@@ -1,68 +1,25 @@
 import sys
-sys.setrecursionlimit(1 << 25)
 read = sys.stdin.readline
 ra = range
 enu = enumerate
 
 
-def exit(*argv, **kwarg):
-    print(*argv, **kwarg)
-    sys.exit()
-
-
-def mina(*argv, sub=1): return list(map(lambda x: x - sub, argv))
-# 受け渡されたすべての要素からsubだけ引く.リストを*をつけて展開しておくこと
-
-
 def a_int(): return int(read())
 
 
-def ints(): return list(map(int, read().split()))
-
-
-def read_col(H):
-    '''H is number of rows
-    A列、B列が与えられるようなとき
-    ex1)A,B=read_col(H)    ex2) A,=read_col(H) #一列の場合'''
-    ret = []
-    for _ in range(H):
-        ret.append(list(map(int, read().split())))
-    return tuple(map(list, zip(*ret)))
-
-
-def read_tuple(H):
-    '''H is number of rows'''
-    ret = []
-    for _ in range(H):
-        ret.append(tuple(map(int, read().split())))
-    return ret
-
-
-def read_matrix(H):
-    '''H is number of rows'''
-    ret = []
-    for _ in range(H):
-        ret.append(list(map(int, read().split())))
-    return ret
-    # return [list(map(int, read().split())) for _ in range(H)] # 内包表記はpypyでは遅いため
-
-
-MOD = 10**9 + 7
-INF = 2**31  # 2147483648 > 10**9
-# default import
-from collections import defaultdict, Counter, deque
-from operator import itemgetter, xor, add
-from itertools import product, permutations, combinations
-from bisect import bisect_left, bisect_right  # , insort_left, insort_right
-from functools import reduce
+from itertools import product
 
 
 # 画像でおいた自分の解説見て
 N = a_int()
 B = []
 for _ in ra(N):
-    A = float(read()[:-1])  # floatなら小数13桁ぐらい入る
-    B.append(int(A * (10**9)))
+    A = read()[:-1]  # floatで扱うと誤差で死ぬ
+    if '.' not in A:
+        A += '.'
+    for _ in range(9 - A[::-1].index('.')):
+        A += '0'
+    B.append(int(A[:-10] + A[-9:]))
 
 N = []
 M = []
@@ -82,11 +39,10 @@ for b in B:
 cnt = [[0] * 19 for _ in ra(19)]
 ans = 0
 for n, m in zip(N, M):
-    x = 18 - n
-    y = 18 - m
+    x = max(18 - n, 0)
+    y = max(18 - m, 0)
     for i, j in product(range(x, 19), range(y, 19)):
         ans += cnt[i][j]
-
     # このデータをcntに追加
     cnt[min(n, 18)][min(m, 18)] += 1
 
