3aa61f6510a255b5b568a9d8d7bd21875956379f
Masaki AOTA <aotamasakimail@gmail.com>
2020-08-23 13:04:55 +0900

    補集合のアイデアと 全探索枝刈り

--- contests/abc176/e/e.py
+++ contests/abc176/e/e.py
@@ -63,3 +63,44 @@ def lcm(a, b):
     # 最小公倍数
     g = gcd(a, b)
     return a // g * b
+
+
+HH, WW, M = ints()
+H, W = read_col(M)
+
+
+def flatten(i, j): return i * WW + j
+
+
+def reshape(x): return x // WW, x % WW
+
+
+H = mina(*H)
+W = mina(*W)
+H_cnt = Counter(H)
+W_cnt = Counter(W)
+# 爆弾がある座標に関しては-1,それ以外はそのまま
+# 爆弾のある座標の具体的な爆破数を持っていれば良い
+
+
+n_bomb = defaultdict(lambda: 0)
+ma = -1
+for h, w in zip(H, W):
+    n_bomb[flatten(h, w)] = H_cnt[h] + W_cnt[w] - 1
+    ma = max(ma, H_cnt[h] + W_cnt[w] - 1)
+
+# 縦横大きい方から見てく
+H_rank = H_cnt.most_common()
+W_rank = W_cnt.most_common()
+# print(H_rank, W_rank)
+
+for i, h_cnt in H_rank:
+    for j, w_cnt in W_rank:
+        if h_cnt + w_cnt <= ma:
+            break  # どうあがいてもこれ以上大きくするのは無理
+        n = n_bomb[flatten(i, j)]
+        if n == 0:
+            n = h_cnt + w_cnt
+        ma = max(ma, n)
+
+print(ma)
